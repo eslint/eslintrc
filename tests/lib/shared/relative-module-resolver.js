0ae0a6027fae76309269e0182f4df18f145c71fe
Richie Bendall <richiebendall@gmail.com>
2021-04-29 00:28:51 +1200

    Chore: Add tests
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

---
+++ tests/lib/shared/relative-module-resolver.js
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview Tests for relative module resolver.
+ */
+"use strict";
+
+const {
+    Legacy: {
+        resolve
+    }
+} = require("../../..");
+const { assert } = require("chai");
+const path = require("path");
+
+describe("resolve", () => {
+    assert.strictEqual(resolve("./file2.js", path.resolve("./tests/fixtures/relative-module-resolver/file.js")), path.resolve("./tests/fixtures/relative-module-resolver/file2.js"));
+});
