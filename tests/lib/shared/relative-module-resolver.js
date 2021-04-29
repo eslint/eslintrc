32039709f3ee41bfaabad15d2e4f7c2d62186e4f
Richie Bendall <richiebendall@gmail.com>
2021-04-29 17:09:54 +1200

    Use better test names
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>

--- tests/lib/shared/relative-module-resolver.js
+++ tests/lib/shared/relative-module-resolver.js
@@ -5,14 +5,22 @@
 
 const {
     Legacy: {
-        ModuleResolver: {
-            resolve
-        }
+        ModuleResolver
     }
 } = require("../../..");
 const { assert } = require("chai");
 const path = require("path");
 
-describe("resolve", () => {
-    assert.strictEqual(resolve("./file2.js", path.resolve("./tests/fixtures/relative-module-resolver/file.js")), path.resolve("./tests/fixtures/relative-module-resolver/file2.js"));
+describe("ModuleResolver", () => {
+    describe("resolve()", () => {
+        it("should correctly resolve a relative path", () => {
+            assert.strictEqual(
+                ModuleResolver.resolve(
+                    "./file2.js",
+                    path.resolve("./tests/fixtures/relative-module-resolver/file.js")
+                ),
+                path.resolve("./tests/fixtures/relative-module-resolver/file2.js")
+            );
+        });
+    });
 });
