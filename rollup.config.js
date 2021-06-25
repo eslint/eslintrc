54238ea88aecd600628287c60a423711efee87ce
Brett Zamir <brettz9@yahoo.com>
2021-06-25 21:18:38 +0800

    New: Add exports; also update devDeps. specify external modules in Rollup config, and align it with eslint-scope

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
 export default {
     input: "./lib/index.js",
-    external: ["fs"],
+    external: [
+        "module", "util", "os", "path", "debug", "fs", "import-fresh",
+        "strip-json-comments", "assert", "ignore", "minimatch", "url", "ajv"
+    ],
+    treeshake: false,
     output: {
         format: "cjs",
-        file: "dist/eslintrc.cjs"
+        file: "dist/eslintrc.cjs",
+        sourcemap: true
     }
 };
