b60c8dcb14eb451ee6e8038326af8580016e95fd
Milos Djermanovic <milos.djermanovic@gmail.com>
2022-04-13 01:59:22 +0200

    fix: use custom Rollup plugin for `import.meta.url`
    Refs eslint/eslint#15766

--- tests/lib/commonjs.cjs
+++ tests/lib/commonjs.cjs
@@ -12,7 +12,8 @@
 const assert = require("assert");
 const eslintrc = require("../../dist/eslintrc.cjs");
 const universal = require("../../dist/eslintrc-universal.cjs");
-
+const path = require("path");
+const sh = require("shelljs");
 
 //------------------------------------------------------------------------------
 // Tests
@@ -71,5 +72,23 @@ describe("eslintrc CommonJS Universal", () => {
         ].forEach(prop => {
             assert.strictEqual(typeof universal.Legacy[prop], "object");
         });
+    });
+});
+
+// https://github.com/eslint/eslint/issues/15766
+describe("eslintrc CommonJS loading", () => {
+    const testDir = path.resolve(__dirname, "../../tmp/my%2Fproject");
+
+    before(() => {
+        sh.mkdir("-p", testDir);
+        sh.cp("", "./dist/eslintrc.cjs", testDir);
+    });
+
+    after(() => {
+        sh.rm("-r", testDir);
+    });
+
+    it("eslintrc.cjs module successfully loads when it is in a path that contains URL-encoded characters", () => {
+        require(path.resolve(testDir, "eslintrc.cjs"));
     });
 });
