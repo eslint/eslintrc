18b15ac0b0150ad0a4f69e61319e5adaea83a9c7
Milos Djermanovic <milos.djermanovic@gmail.com>
2022-04-21 14:22:19 +0200

    fix: use custom Rollup plugin for `import.meta.url` (#77)
    * fix: use custom Rollup plugin for `import.meta.url`
    
    Refs eslint/eslint#15766
    
    * Use URL#toString()
    
    Co-authored-by: 唯然 <weiran.zsd@outlook.com>
    
    Co-authored-by: 唯然 <weiran.zsd@outlook.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,22 @@
+/**
+ * Custom Rollup plugin for `import.meta.url` transformation to commonjs.
+ * The default transformation ('file:' + __filename) does not check characters in __filename,
+ * and thus can produce invalid URLs, like in https://github.com/eslint/eslint/issues/15766
+ * See https://github.com/eslint/eslint/issues/15766#issuecomment-1093941321
+ * @returns {Object} Rollup plugin object.
+ */
+function importMetaURLPlugin() {
+    return {
+        name: "custom-import-meta-url",
+        resolveImportMeta(property) {
+            if (property === "url") {
+                return "require('url').pathToFileURL(__filename).toString()";
+            }
+            return null;
+        }
+    };
+}
+
 export default [
     {
         input: "./lib/index.js",
@@ -12,7 +31,8 @@ export default [
             file: "dist/eslintrc.cjs",
             sourcemap: true,
             freeze: false
-        }
+        },
+        plugins: [importMetaURLPlugin()]
     },
     {
         input: "./lib/index-universal.js",
